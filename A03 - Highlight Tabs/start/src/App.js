6c373066218fb8106d506dfa3bec08e73b02e401
cockochan <56898295+cockochan@users.noreply.github.com>
2020-08-04 19:19:27 +0300

    cool

--- A03 - Highlight Tabs/start/src/App.js
+++ A03 - Highlight Tabs/start/src/App.js
@@ -1,14 +1,54 @@
-import React, {useState} from 'react';
+import React from 'react';
+import Tab from'./components/Tab'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink 
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Features from './pages/Features';
+function App() {
+  
 
-export default function App() {
-  const [markdown, setMarkdown] = useState('# sup')
+    
+
   return (
+    <Router >
     <div className="app">
-
-        <div className="viewport">{markdown}</div>
-     
+      <div className="browser">
+        <div className="tabs">
+         
+          <Tab> <NavLink activeClassName='is-active' to="/" exact={true}>Home</NavLink ></Tab>
+          <Tab> <NavLink activeClassName='is-active' to="/about">About</NavLink ></Tab>
+          <Tab> <NavLink activeClassName='is-active' to="/features">Features</NavLink ></Tab>
+          
         </div>
+
+        <div className="viewport">
+          <Switch>
+        
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/features">
+            <Features />
+          </Route>
+          <Route path="/" exact={true}>
+            <Home />
+          </Route>
+          </Switch>
+        </div>
+     
+          </div>
+    </div>
+   
+         
+   
+    </Router>
   );
 }
 
+export default App;
